3c8307cd48b50ec97ad98753a51f05b727675606
DiogoKitahara <diogokitahara@gmail.com>
2024-03-27 17:56:45 -0300

    added

--- JavaScript/Lista3/funcoes.js
+++ JavaScript/Lista3/funcoes.js
@@ -1,23 +1,39 @@
 function calcularMedia(p1, p2) {
     var media = ((p1 * 4) + (p2 * 6)) / 10;
     media_aluno.value = media;
-    return media;
 }
 
-function calcularSituacao(p1, p2, falta) {
-    var m = calcularMedia(p1, p2);
+function calcularSituacao(p1, p2, falta) { 
+    var m = ((p1 * 4) + (p2 * 6)) / 10;
     if (m >= 6.0 && falta <= 16) {
-        situacao_aluno.value = 1;
+        situacao_aluno.value = "Aprovado";
     }
     else {
-        situacao_aluno.value = 2;
+        situacao_aluno.value = "Reprovado";
     }
 }
 
 function carros(custo) {
+    custo = parseFloat(custo);
     valor_final.value = custo + (custo * 0.3);
 }
 
 function ordenar(n1, n2, n3) {
-
+    var numeros = [n1, n2, n3], aux;
+    if (numeros[0] > numeros[2]) {
+        aux = numeros[2];
+        numeros[2] = numeros[0];
+        numeros[0] = aux;
+    }
+    if (numeros[0] > numeros[1]) {
+        aux = numeros[1];
+        numeros[1] = numeros[0];
+        numeros[0] = aux;
+    }
+    if (numeros[1] > numeros[2]) {
+        aux = numeros[2];
+        numeros[2] = numeros[1];
+        numeros[1] = aux;
+    }
+    numero_ordem.value = numeros;
 }
